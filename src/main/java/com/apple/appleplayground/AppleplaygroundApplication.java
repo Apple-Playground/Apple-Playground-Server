c765fc54860bdea046673bcf93f34dfac917f445
snowykte0426 <snowykte0426@naver.com>
2025-06-11 12:22:21 +0900

    feat: enable Feign clients in Spring Boot application

--- src/main/java/com/apple/appleplayground/AppleplaygroundApplication.java
+++ src/main/java/com/apple/appleplayground/AppleplaygroundApplication.java
@@ -2,8 +2,10 @@ package com.apple.appleplayground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AppleplaygroundApplication {
 
 	public static void main(String[] args) {
